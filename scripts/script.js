d8fcfe36f08b415d01c667979ee28128b118b0b2
Alex <lathamwyse123@gmail.com>
2022-11-22 16:22:09 -0800

    asdasd

--- scripts/script.js
+++ scripts/script.js
@@ -26,3 +26,31 @@ function buttonPress() {
 }
 
 ClickButton.addEventListener("click", buttonPress)
+
+function updateImage(event) {
+    let image = document.getElementById("shoppingCart")
+    image.src = "image/263142.png";
+    image.alt = "shopping cart"
+    image.width = "300"; 
+    image.height = "300";
+    console.log(event);   
+}
+
+ClickButton.addEventListener("click", updateImage)
+
+
+const buttonContainer = document.querySelector(".buttons")
+
+function changeBGGreen(event) 
+{
+    if (event.target.tagName === "BUTTON")
+    event.target.style.backgroundColor = "green";
+}
+
+function changeColor(event) {
+    event.target.style.backgroundColor = event.target.textContent;
+}
+
+buttonContainer.addEventListener("mouseover", changeBGGreen)
+buttonContainer.addEventListener("click", changeColor)
+
